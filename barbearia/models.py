409cacb967eebcb306db0a68234054b772b99130
Tiago Cabral <tiagocabral.gja@gmail.com>
2026-07-07 19:28:49 -0300

    modificação nos models

--- barbearia/models.py
+++ barbearia/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
-
+from django.contrib.auth.models import User
+from datetime import timedelta
 
 # Create your models here.
 
@@ -10,14 +11,20 @@ class Category(models.Model):
         verbose_name_plural = 'Categories'
     
     name = models.CharField(max_length=50)
+    duracao = models.DurationField(default=timedelta(minutes=30))
 
     def __str__(self):
         return self.name
 
 class Agendamento(models.Model):
-    date = models.DateTimeField(default=timezone.now)   
+    date = models.DateTimeField(default=timezone.now)  
     category = models.ForeignKey(
         Category, 
         blank=True,
         on_delete=models.SET_NULL, null=True
         )
+    cliente = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        blank=True, null=True
+    )
